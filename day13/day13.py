4f42b0aa86422580e216a92fe48f355fee47d050
Steffen Maus <steffenmaus@users.noreply.github.com>
2024-12-13 14:59:38 +0100

    Day13 changed approaches.

--- day13/day13.py
+++ day13/day13.py
@@ -1,7 +1,9 @@
+import re
+
 from z3 import *
 
 with open('input.txt') as file:
-    lines = [line.rstrip() for line in file]
+    ints = [[int(n) for n in re.findall(r'-?\d+', line)] for line in file]
 
 
 def get_groups_from_lines(lines):
@@ -17,15 +19,79 @@ def get_groups_from_lines(lines):
     return groups
 
 
-groups = get_groups_from_lines(lines)
+groups = get_groups_from_lines(ints)
+
+print("Linear algebra approach:")
+
+
+def calc(target, a, b):
+    px, py = target
+    ax, ay = a
+    bx, by = b
+
+    bcount = (py * ax - px * ay) / (by * ax - bx * ay)
+    acount = (px - bcount * bx) / ax
+
+    if acount % 1 == 0 and bcount % 1 == 0:
+        return 3 * int(acount) + int(bcount)
+    else:
+        return 0
+
+
 p1 = 0
 p2 = 0
+for g in groups:
+    a, b, p = g
+    ax, ay = a
+    bx, by = b
+    px, py = p
+    p1 += calc((px, py), (ax, ay), (bx, by))
+    p2 += calc((px + 10000000000000, py + 10000000000000), (ax, ay), (bx, by))
+
+print("Part 1: " + str(p1))
+print("Part 2: " + str(p2))
+print("---")
+
+print("Solver approach:")
+
+
+def solve(target, a, b):
+    my_solver = z3.Solver()
+    px, py = target
+    ax, ay = a
+    bx, by = b
+
+    acount = Int('acount')
+    bcount = Int('bcount')
+    my_solver.add(px == ax * acount + bx * bcount)
+    my_solver.add(py == ay * acount + by * bcount)
+    if my_solver.check() == z3.sat:
+        model = my_solver.model()
+        return model.eval(acount).as_long() * 3 + model.eval(bcount).as_long()
+    else:
+        return 0
+
+
+p1 = 0
+p2 = 0
+for g in groups:
+    a, b, p = g
+    ax, ay = a
+    bx, by = b
+    px, py = p
+    p1 += solve((px, py), (ax, ay), (bx, by))
+    p2 += solve((px + 10000000000000, py + 10000000000000), (ax, ay), (bx, by))
+
+print("Part 1: " + str(p1))
+print("Part 2: " + str(p2))
+print("---")
+
+print("DP approach (will fail on part 2):")
 
 mem = {}
 
 
-# not needed anymore, can still solve part 1
-def f(target, current, a, b, cost):
+def dp(target, current, a, b, cost):
     px, py = target
     cx, cy = current
     ax, ay = a
@@ -38,53 +104,21 @@ def f(target, current, a, b, cost):
     if current[0] > target[0] or current[1] > target[1]:
         return sys.maxsize
 
-    na = f(target, (cx + ax, cy + ay), a, b, cost + 3)
-    nb = f(target, (cx + bx, cy + by), a, b, cost + 1)
+    na = dp(target, (cx + ax, cy + ay), a, b, cost + 3)
+    nb = dp(target, (cx + bx, cy + by), a, b, cost + 1)
 
     mem[(px - cx, py - cy, a, b)] = min(na, nb)
     return min(na, nb)
 
 
-def f2(target, a, b):
-    my_optimizer = z3.Optimize()
-    px, py = target
-    ax, ay = a
-    bx, by = b
-
-    acount = Int('acount')
-    bcount = Int('bcount')
-    wins = Int('wins')
-
-    win = 0
-    win += If(px == ax * acount + bx * bcount, 1, 0)
-    win += If(py == ay * acount + by * bcount, 1, 0)
-    my_optimizer.add(wins == win)
-
-    my_optimizer.maximize(wins)
-    my_optimizer.minimize(acount * 3 + bcount)
-    my_optimizer.check()
-    model = my_optimizer.model()
-    w = model.eval(wins).as_long()
-    if w == 2:
-        return model.eval(acount).as_long() * 3 + model.eval(bcount).as_long()
-    else:
-        return sys.maxsize
-
-
+p1 = 0
 for g in groups:
     a, b, p = g
-    ax = int(a.split("X+")[1].split(",")[0])
-    ay = int(a.split("Y+")[1])
-    bx = int(b.split("X+")[1].split(",")[0])
-    by = int(b.split("Y+")[1])
-    px = int(p.split("X=")[1].split(",")[0])
-    py = int(p.split("Y=")[1])
-    res = f((px, py), (0, 0), (ax, ay), (bx, by), 0)
+    ax, ay = a
+    bx, by = b
+    px, py = p
+    res = dp((px, py), (0, 0), (ax, ay), (bx, by), 0)
     if res < sys.maxsize:
         p1 += res
-    res = f2((px + 10000000000000, py + 10000000000000), (ax, ay), (bx, by))
-    if res < sys.maxsize:
-        p2 += res
-
 print("Part 1: " + str(p1))
-print("Part 2: " + str(p2))
+
