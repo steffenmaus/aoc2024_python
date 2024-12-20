08bf0c47491e9b86c5d56ead8efc2b80d617ba9c
Steffen Maus <steffenmaus@users.noreply.github.com>
2024-12-20 07:40:52 +0100

    Day20 improved performance.

--- day20/day20.py
+++ day20/day20.py
@@ -1,25 +1,5 @@
-import heapq
-from collections import defaultdict
-
 with open('input.txt') as file:
     lines = [line.rstrip() for line in file]
-
-
-def dijkstra(start, distances):
-    out = {}
-    out[start] = 0
-    Q = []
-    for nei in distances[start]:
-        node, dist = nei
-        heapq.heappush(Q, (dist, node, start))
-    while len(out.keys()) != len(distances.keys()):
-        dist, current, prev = heapq.heappop(Q)
-        if current not in out.keys():
-            out[current] = dist
-            for nei in distances[current]:
-                n, d = nei
-                heapq.heappush(Q, (d + dist, n, current))
-    return out
 
 
 def man_dist(p1, p2):
@@ -31,12 +11,42 @@ def get_all_nei_2d_4(p):
     r = [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]
     return r
 
+def get_all_nei_within_man_dist(p, max_dist):
+    x, y = p
+    out = set()
+    for d in range(1, max_dist + 1):
+        for dx in range(d+1):
+            dy = d - dx
+            out.add((x + dx, y + dy))
+            out.add((x + dx, y - dy))
+            out.add((x - dx, y + dy))
+            out.add((x - dx, y - dy))
+    return out
+
+def steps_in_maze(start):
+    out = {}
+    border = set()
+    border.add(start)
+    completed = set()
+    steps = 0
+    while border:
+        next_border = set()
+        completed.update(border)
+        for p in border:
+            out[p] = steps
+            for n in get_all_nei_2d_4(p):
+                if n in floor and n not in completed:
+                    next_border.add(n)
+        border = next_border
+        steps += 1
+    return out
+
 
 def f(cheat_time):
     out = 0
     for p in floor:
-        for p2 in floor:
-            if p != p2 and man_dist(p, p2) <= cheat_time:
+        for p2 in get_all_nei_within_man_dist(p, cheat_time):
+            if p != p2 and p2 in floor:
                 if distances_from_start[p] + distances_to_end[p2] + man_dist(p, p2) <= best_distance - 100:
                     out += 1
     return out
@@ -59,17 +69,14 @@ for y in range(0, Y):
                 start = p
                 floor.add(p)
 
-distances = defaultdict(list)
-for p in floor:
-    for n in get_all_nei_2d_4(p):
-        distances[p].append((n, 1))
 
-distances_from_start = dijkstra(start, distances)
-distances_to_end = dijkstra(target, distances)
+
+distances_from_start = steps_in_maze(start)
+distances_to_end = steps_in_maze(target)
 best_distance = distances_from_start[target]
 
 p1 = f(2)
-print("Part 1: " + str(p1))
-
 p2 = f(20)
+
+print("Part 1: " + str(p1))
 print("Part 2: " + str(p2))
