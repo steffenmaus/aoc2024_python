95415c808b52aff137508db570c5cc5057e81deb
Steffen Maus <steffenmaus@users.noreply.github.com>
2024-12-23 07:24:21 +0100

    Day23 replaced part 1 with generic solution.

--- day23/day23.py
+++ day23/day23.py
@@ -8,21 +8,39 @@ def get_network_id(comps):
     return ",".join(sorted(comps))
 
 
-mem = {}
+mem_f = {}
 
 
-def f(completed, candidates):
+def f(completed, candidates, target_size):
     completed_id = get_network_id(completed)
-    if completed_id in mem.keys():
-        return mem[completed_id]
+    if completed_id in mem_f.keys():
+        return mem_f[completed_id]
+    out = set()
+    if len(completed) == target_size:
+        return {get_network_id(completed)}
+    if not candidates:
+        return {}
+    for c in candidates:
+        for r in f(completed.union({c}), candidates.intersection(connections[c]), target_size):
+            out.add(r)
+    mem_f[completed_id] = out
+    return out
+
+
+mem_g = {}
+
+
+def g(completed, candidates):
+    completed_id = get_network_id(completed)
+    if completed_id in mem_g.keys():
+        return mem_g[completed_id]
     out = set()
     if not candidates:
         return {get_network_id(completed)}
-
     for c in candidates:
-        for r in f(completed.union({c}), candidates.intersection(connections[c])):
+        for r in g(completed.union({c}), candidates.intersection(connections[c])):
             out.add(r)
-    mem[completed_id] = out
+    mem_g[completed_id] = out
     return out
 
 
@@ -36,11 +54,8 @@ for l in lines:
 lans = set()
 for c in connections.keys():
     if c.startswith("t"):
-        for n in connections[c]:
-            for m in connections[c]:
-                if m in connections[n]:
-                    lans.add(get_network_id([c, n, m]))
-
+        for r in f({c}, set(connections[c]), 3):
+            lans.add(r)
 p1 = len(lans)
 
 print("Part 1: " + str(p1))
@@ -48,7 +63,7 @@ print("Part 1: " + str(p1))
 bestSize = 0
 p2 = None
 for c in connections.keys():
-    for r in f({c}, set(connections[c])):
+    for r in g({c}, set(connections[c])):
         if len(r) > bestSize:
             bestSize = len(r)
             p2 = r
